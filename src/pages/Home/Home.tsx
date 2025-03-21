a4099358b651c0f1426ae030158071d0173112cb
Bruno Gomes <bgomes.89@gmail.com>
2025-03-21 15:09:02 -0300

    fix: ajuste bg grid de imagens

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,7 +8,7 @@ import img3 from '../../assets/Image3.png'
 
 const Home = () => {
   return (
-    <section className="w-full h-[calc(100svh-114px)] bg-quaternary">
+    <section className="w-full h-svh bg-quaternary">
       <div className="max-w-[1440px] p-4 flex justify-center items-center m-auto">
         <div className="w-[413px] h-[120px] mt-32 flex flex-col gap-6 space-y-5 justify-center items-center relative">
           <h1 className="text-5xl font-homeKalita text-primary text-center">
@@ -20,11 +20,11 @@ const Home = () => {
           <Button />
         </div>
       </div>
-      <div className='max-w-[1440px] h-[550px] m-auto mt-10 flex flex-row justify-between gap-4'>
+      <div className='max-w-[1440px] m-auto mt-10 flex flex-row justify-between gap-4 overflow-hidden bg-quaternary'>
         <div>
           <img src={img} alt="" />
         </div>
-        <div>
+        <div > 
           <img src={img3} alt="" />
         </div>
         <div className='flex flex-col gap-4'>
