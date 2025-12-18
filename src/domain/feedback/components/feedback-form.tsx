c743b9a6a794ada94b4defac4932da1af6a5febe
CaioMiranda12 <caioclm04@gmail.com>
2025-12-18 11:42:49 -0300

    feat: add react-hook-form on FeedbackForm component

--- src/domain/feedback/components/feedback-form.tsx
+++ src/domain/feedback/components/feedback-form.tsx
@@ -1,51 +1,72 @@
+'use client'
 
+import Input from "@/app/shared/ui/form/input";
+import { useFeedbackForm } from "../hooks/useFeedbackForm";
+import { FeedbackSchema, FeedbackSchemaType } from "../schemas/feedbackSchema";
+import { Textarea } from "./Textarea";
+import { AuthorizationInput } from "./AuthorizationInput";
 
 export function FeedbackForm() {
+  const {
+    handleSubmit,
+    errors,
+    control,
+    reset: resetForm,
+    clearErrors
+  } = useFeedbackForm(FeedbackSchema);
+
+  const onSubmit = (formData: FeedbackSchemaType) => {
+    console.log(formData)
+  }
+
   return (
-    <div className="flex flex-col gap-10 bg-kalita-bg-medium py-8 px-12 border border-kalita-bg-light-brown rounded-lg">
-      <div className="flex flex-col gap-3">
-        <label className="text-base font-nunito font-semibold text-kalita-brown-dark leading-normal">Seu nome</label>
-        <input
-          type="text"
-          placeholder="Digite seu nome"
-          className="h-[52px] px-8 border border-kalita-bg-light-brown bg-kalita-bg-light placeholder:text-kalita-bg-light-brown placeholder:text-sm rounded-sm"
-        />
-      </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-10 bg-kalita-bg-medium py-8 px-12 border border-kalita-bg-light-brown rounded-lg max-w-[520px]"
+    >
+      <Input
+        name="name"
+        control={control}
+        errors={errors}
+        nameInput="Seu nome"
+        nameLabelInput="name"
+        namePlaceholderInput="Digite seu nome"
+        typeInput="text"
+      />
 
-      <div className="flex flex-col gap-3">
-        <label className="text-base font-nunito font-semibold text-kalita-brown-dark leading-normal">Qual foi o tipo de sessão?</label>
-        <input
-          type="text"
-          placeholder="Parto, acompanhamento do bebê, gestante..."
-          className="h-[52px] px-8 border border-kalita-bg-light-brown bg-kalita-bg-light placeholder:text-kalita-bg-light-brown placeholder:text-sm rounded-sm"
-        />
-      </div>
+      <Input
+        name="sessionType"
+        control={control}
+        errors={errors}
+        nameInput="Qual foi o tipo de sessão?"
+        nameLabelInput="sessionType"
+        namePlaceholderInput="Parto, acompanhamento do bebê, gestante..."
+        typeInput="text"
+      />
 
-      <div className="flex flex-col gap-3">
-        <label className="text-base font-nunito font-semibold text-kalita-brown-dark leading-normal">Seu depoimento:</label>
-        <textarea
-          placeholder="Descreva aqui a sua experiência..."
-          className="h-[140px] px-8 py-4 border border-kalita-bg-light-brown bg-kalita-bg-light placeholder:text-kalita-bg-light-brown placeholder:text-sm resize-none rounded-sm"
-        />
-      </div>
 
-      <div className="flex justify-center items-center gap-[13px] px-4 py-[22px] bg-kalita-brown-dark rounded-sm">
-        <input
-          type="checkbox"
-          width={18}
-          height={18}
-          className="h-[18px] w-[18px]"
-        />
-        <p className="text-kalita-bg-light text-sm font-nunito">
-          Autorizo a publicação deste feedback no site Kálita Fotografias. Entendo que meu nome e depoimento poderão ser exibidos publicamente.
-        </p>
-      </div>
+      <Textarea
+        name="feedback"
+        nameInput="Seu depoimento:"
+        namePlaceholderInput="Descreva aqui a sua experiência..."
+        control={control}
+        errors={errors}
+      />
+
+      <AuthorizationInput
+        name="authorization"
+        namePlaceholderInput="Autorizo a publicação deste feedback no site Kálita Fotografias. Entendo que meu nome e depoimento poderão ser exibidos publicamente."
+        control={control}
+        errors={errors}
+      />
+
 
       <button
+        type="submit"
         className="bg-kalita-brown-medium text-kalita-bg-light h-[52px] hover:bg-kalita-brown-dark cursor-pointer rounded-md"
       >
         Enviar
       </button>
-    </div>
+    </form>
   )
 }
