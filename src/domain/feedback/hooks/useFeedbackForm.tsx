2d8cd73064893e571e39f2b893e203e635b6dbae
CaioMiranda12 <caioclm04@gmail.com>
2025-12-18 11:42:31 -0300

    feat: create useFeedbackForm

---
+++ src/domain/feedback/hooks/useFeedbackForm.tsx
@@ -0,0 +1,30 @@
+import { useForm } from "react-hook-form";
+import { FeedbackSchema, FeedbackSchemaType } from "../schemas/feedbackSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const useFeedbackForm = (schema: typeof FeedbackSchema) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    clearErrors,
+    formState: { errors }
+  } = useForm<FeedbackSchemaType>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      sessionType: "",
+      feedback: ""
+    }
+  });
+
+  return {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    errors,
+    clearErrors
+  };
+};
