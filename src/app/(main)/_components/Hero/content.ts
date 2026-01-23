423a86ec88eb85a4272ce30c6abd64d31406816a
dinhoSiwa <claudiosilva.one@gmail.com>
2026-01-22 21:57:51 -0300

    refactor: update-url-path

--- src/app/(main)/_components/Hero/content.ts
+++ src/app/(main)/_components/Hero/content.ts
@@ -1,17 +1,17 @@
   export const slidePayload = [
     {
       alt: 'Pai e mãe segurando o recém-nascido',
-      source: './pais-com-recem-nascido-na-maternidade.svg',
+      source: 'https://res.cloudinary.com/dda8z5n1i/image/upload/v1769124417/Rectangle_24_t1wqab.svg',
       mobile: true
     },
     {
       alt: 'Pernas de um recém-nascido na maternidade',
-      source: './pernas-de-um-recem-nascido-na-maternidade.svg', 
+      source: 'https://res.cloudinary.com/dda8z5n1i/image/upload/v1769122767/Foto_tablet-1_pfng6f.jpg', 
       mobile: false
     },
     {
       alt: 'Mãe com seu filho recém-nascido na maternidade',
-      source: './mae-com-seu-filho-recem-nascido-na-maternidade.svg',
+      source: 'https://res.cloudinary.com/dda8z5n1i/image/upload/v1769122767/Foto_tablet-2_eylhre.jpg',
       mobile: false
     }
   ];
